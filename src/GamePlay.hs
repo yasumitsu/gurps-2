87a439aea1057a98f4d522c17582192068659007
joachifm <joachim.fasting@gmail.com>
2013-08-09 12:14:19 +0200

    GamePlay: add copyright notice for GURPS Lite

--- src/GamePlay.hs
+++ src/GamePlay.hs
@@ -4,6 +4,9 @@ Module : GamePlay
 Types and functions for simulating GURPS gameplay.
 
 All rules are from the GURPS 4th Lite book.
+
+GURPS Lite is (C) by Steve Jackson Games Inc., and may be downloaded at
+no cost from <www.sjgames.com/gurps/lite/>.
 -}
 
 module GamePlay
